f5c0ab5f824aa302d293109d9a563de17eb22b64
Black_Tiger <115871104+ircorona@users.noreply.github.com>
2026-01-18 21:50:18 -0600

    Update pruebas.js

--- scripts/pruebas.js
+++ scripts/pruebas.js
@@ -1,12 +1,15 @@
+document.getElementById("heading1").innerHTML = "Hola, este es un ejemplo de JavaScript.";
+
+
 // Esperar a que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
     
     // Verificar que el elemento existe antes de modificarlo
-    const heading = document.getElementById("heading1");
+    //const heading = document.getElementById("heading1");
     
-    if (heading) {
-        heading.innerHTML = "Hola, este es un ejemplo de JavaScript.";
-    }
+    //if (heading) {
+   //     heading.innerHTML = "Hola, este es un ejemplo de JavaScript.";
+   // }
     
     // Redirigir a otra página web - COMENTADO
     // window.location.href = "https://carlos.sanchezdonate.com";
