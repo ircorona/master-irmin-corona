ace1e7816a62585830460e0871deb80de58a77cc
Black_Tiger <115871104+ircorona@users.noreply.github.com>
2026-01-19 22:01:15 -0600

    unica.php

---
+++ unica.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <title>This is an example | climbthesearches.com </title>
+        <meta name="description" content="blablabla" />
+
+    </head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+        <h1>Unica Page</h1>
+    <footer>
+
+    </footer>
+
+</body>
