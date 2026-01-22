493ae60bb4d8ae1d6aab7e53000d38a43a04797f
Black_Tiger <115871104+ircorona@users.noreply.github.com>
2026-01-21 22:12:10 -0600

    09 – 03 Meta Robots

--- unica.php
+++ unica.php
@@ -5,6 +5,17 @@
         <meta charset="UTF-8" />
         <title>This is an example | climbthesearches.com </title>
         <meta name="description" content="blablabla" />
+        <meta name="robots" content="all, noarchieve, nositelinkssearchbox, nosnippet, max-snippet:20, notranslate " />
+        <meta name="Googlebot" content="all" />
+        <!--
+        <meta name="robots" content="unavailable_after: 2020-09-21" />
+        <meta name="robots" content="all" />
+        <meta name="robots" content="max-image-preview:standard" />
+        <meta name="robots" content="index, nofollow" />
+        <meta name="robots" content="index, follow" />
+        <meta name="robots" content="none" />
+        <meta name="robots" content="max-snippet:-1" /> -- cabe todo el snippet completo pero si es 0 = nosnippet
+        -->
 
     </head>
 
