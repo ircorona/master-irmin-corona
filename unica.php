481cd2041eac323f1cbdbb8b75a9b7face5fd717
Black_Tiger <115871104+ircorona@users.noreply.github.com>
2026-01-22 22:01:12 -0600

    Create unica.php

---
+++ unica.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- <meta http-equiv="refresh " content="30; url=https://google.com/" /> -->
+        <!-- <meta http-equiv="refresh " content="3" /> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <title>This is an example | climbthesearches.com </title>
+        <meta name="description" content="blablabla" />
+        <meta name="robots" content="all, noarchieve, nositelinkssearchbox, nosnippet, max-snippet:20, notranslate " />
+        <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27">
+        <!--
+        <meta name="robots" content="unavailable_after: 2020-09-21" />
+        <meta name="robots" content="all" />
+        <meta name="robots" content="max-image-preview:standard" />
+        <meta name="robots" content="index, nofollow" />
+        <meta name="robots" content="index, follow" />
+        <meta name="robots" content="none" />
+        <meta name="robots" content="max-snippet:-1" /> -- cabe todo el snippet completo pero si es 0 = nosnippet
+        
+        -->
+        <meta name="raiting" content="adult" />
+
+    </head>
+
+<body>
+    <header>
+        <nav></nav>
+    </header>
+        <h1>Unica Page</h1>
+    <footer>
+
+    </footer>
+
+</body>
