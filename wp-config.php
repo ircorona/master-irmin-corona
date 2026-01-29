563ae2874b17a0649c65829ef6133afbe9d9f11e
Black_Tiger <115871104+ircorona@users.noreply.github.com>
2026-01-29 06:51:00 -0600

    installed plugin acf

--- wp-config.php
+++ wp-config.php
@@ -6,7 +6,7 @@
 // ** Database settings ** //
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8mb4' );
 define( 'DB_COLLATE', '' );
